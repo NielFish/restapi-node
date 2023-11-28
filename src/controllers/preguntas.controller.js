f5027a8b350a0059a39663a6a4492bedd591a42f
Daniel Lopez <110749509+NielFish@users.noreply.github.com>
2023-11-28 09:20:00 -0600

    Update createPreguntas

--- src/controllers/preguntas.controller.js
+++ src/controllers/preguntas.controller.js
@@ -54,47 +54,48 @@ export const getPregunta = async (req, res) => {
     }
 }
 
+export const getPregunta = async (req, res) => {
+    try{
+        const [rows] = await pool.query('SELECT * FROM preguntas WHERE id = ?', [req.params.id])
+
+        if(rows.length <= 0) return res.status(404).json({message: 'Pregunta no encontrada'})
+
+        res.json(rows[0])
+    }catch (error){
+        return res.status(500).json({
+            message: 'Error interno'
+        })
+    }
+}
+
 export const createPreguntas = async (req, res) => {
     try {
         const { categoria, pregunta, respuesta, incorrecta1, incorrecta2, incorrecta3 } = req.body;
 
         if (!categoria || !pregunta || !respuesta || !incorrecta1 || !incorrecta2 || !incorrecta3) {
             return res.status(400).json({
-                success: false,
-                data: [],
-                message: 'Todos los campos son obligatorios',
-                errors: "",
-                rows: 0
+                message: 'Todos los campos son obligatorios'
             });
         }
 
         const [rows] = await pool.query('INSERT INTO preguntas (categoria, pregunta, respuesta, incorrecta1, incorrecta2, incorrecta3) VALUES (?,?,?,?,?,?)', [categoria, pregunta, respuesta, incorrecta1, incorrecta2, incorrecta3]);
-
-        res.json({
-            success: true,
-            data: {
-                id: rows.insertId,
-                categoria,
-                pregunta,
-                respuesta,
-                incorrecta1,
-                incorrecta2,
-                incorrecta3
-            },
-            message: "Pregunta creada exitosamente",
-            errors: "",
-            rows: 1
+        
+        res.send({
+            id: rows.insertId,
+            categoria,
+            pregunta,
+            respuesta,
+            incorrecta1,
+            incorrecta2,
+            incorrecta3
         });
     } catch (error) {
         return res.status(500).json({
-            success: false,
-            data: [],
-            message: 'Error interno',
-            errors: error.message,
-            rows: 0
+            message: 'Error interno'
         });
     }
 }
+
 
 export const updatePreguntas = async (req, res) => {
     try {
